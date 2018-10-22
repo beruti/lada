6b34ae91b51f59468e7a164edc68e675a4c8ee89
Innes <ainnes@uk167363>
2018-10-22 08:27:13 +0100

    first commit, landing Grid in place

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$("h3").css("color", "purple")
+
+
+var title = new Vue({
+  el: '#title',
+  data: {
+    heading: 'The Lada Rally: 5 good words of intrigue'
+  }
+})
+
+
+var title = new Vue({
+  el: '#landingSubtitle',
+  data: {
+    paragraph: 'Forty years after the publication of his groundbreaking travelogue, ‘‘In Patagonia,’’ the author’s writing — and style — have lost none of their power to bewitch and inspire.'
+  }
+})
+
+
+var app = new Vue({
+  el: '#intro',
+  data: {
+    paragraph: 'Thursday. On recommendation of the organisers we turned up to a lot near the airport. With a budget of 750 euros, a graveyard of broken windscreens, punctured tyres and unregistered vehicles awaited us. But in this rough, past a dozen deformed hatchbacks and a promising people carrier, lay our diamond, a crusty BMW 3 series with a chassis so low to the ground that you\'d think it was returning the earth from which it came. The vehicle of the garage owner\'s lackey, he\'d resurrected it from the scrapheap, giving it strobe headlights and filling the boot with spoils from fishing trips but critically, he\'d removed its suspension and as a result each unannounced bump in the road to come was enough to produce a yelp and cold sweat from each of us. A few late arrivals were circling though, eyeing up our car and with time running out we made our prayers and sealed the deal on our mobile home for the next week. With our newly adopted vehicle, from one Saturday to the next we had to go coast to coast, from Tallinn in Estonia to Thessaloniki in Greece. As we left the garage, a team were huddled round their newly purchased Mercedes E Class, trying to stop a flow of coolant as it sprayed onto the tarmac.'
+  }
+})
