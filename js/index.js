acb46df597543008b854910d035ca68c4fee2abb
Innes <ainnes@uk167363>
2018-10-22 14:46:31 +0100

    mock1 complete

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $("h3").css("color", "purple")
 var title = new Vue({
   el: '#title',
   data: {
-    heading: 'The Lada Rally: 5 good words of intrigue'
+    heading: 'The Lada Rally: All Horsepower Corrupts'
   }
 })
 
@@ -12,14 +12,18 @@ var title = new Vue({
 var title = new Vue({
   el: '#landingSubtitle',
   data: {
-    paragraph: 'Forty years after the publication of his groundbreaking travelogue, ‘‘In Patagonia,’’ the author’s writing — and style — have lost none of their power to bewitch and inspire.'
+    paragraph: 'A scrape through Eastern Europe. 13 countries and 7 days.'
   }
 })
 
-
-var app = new Vue({
-  el: '#intro',
+var paras = new Vue({
+  el: '#articleContent',
   data: {
-    paragraph: 'Thursday. On recommendation of the organisers we turned up to a lot near the airport. With a budget of 750 euros, a graveyard of broken windscreens, punctured tyres and unregistered vehicles awaited us. But in this rough, past a dozen deformed hatchbacks and a promising people carrier, lay our diamond, a crusty BMW 3 series with a chassis so low to the ground that you\'d think it was returning the earth from which it came. The vehicle of the garage owner\'s lackey, he\'d resurrected it from the scrapheap, giving it strobe headlights and filling the boot with spoils from fishing trips but critically, he\'d removed its suspension and as a result each unannounced bump in the road to come was enough to produce a yelp and cold sweat from each of us. A few late arrivals were circling though, eyeing up our car and with time running out we made our prayers and sealed the deal on our mobile home for the next week. With our newly adopted vehicle, from one Saturday to the next we had to go coast to coast, from Tallinn in Estonia to Thessaloniki in Greece. As we left the garage, a team were huddled round their newly purchased Mercedes E Class, trying to stop a flow of coolant as it sprayed onto the tarmac.'
+    paragraphs: [
+      { content: 'Thursday. On recommendation of the organisers we turned up to a lot near the airport. With a budget of 750 euros, a graveyard of broken windscreens, punctured tyres and unregistered vehicles awaited us. But in this rough, past a dozen deformed hatchbacks and a promising people carrier, lay our diamond, a crusty BMW 3 series with a chassis so low to the ground that you\'d think it was returning the earth from which it came. The vehicle of the garage owner\'s lackey, he\'d resurrected it from the scrapheap, giving it strobe headlights and filling the boot with spoils from fishing trips but critically, he\'d removed its suspension and as a result each unannounced bump in the road to come was enough to produce a yelp and cold sweat from each of us. A few late arrivals were circling though, eyeing up our car and with time running out we made our prayers and sealed the deal on our mobile home for the next week. With our newly adopted vehicle, from one Saturday to the next we had to go coast to coast, from Tallinn in Estonia to Thessaloniki in Greece. As we left the garage, a team were huddled round their newly purchased Mercedes E Class, trying to stop a flow of coolant as it sprayed onto the tarmac.' },
+      { content: '9AM Saturday morning: everyone assembled in a potholed carpark off Estonia’s equivalent of the M4. Ten cracked-up cars and their teams, applying racing stripes and whispering final blessings to their motors. A golden VW Passat nearly didn’t make it to the start line, having had to ask for a jump start from some kindly Estonian en route. A people carrier careens and donuts round and out of the starting gates.'},
+      { content: 'About fifty hours of driving lay ahead of us as we set out from that carpark. Looking on a map, with a big x through Russia, a decision would be needed as we came to the bottom of Poland. We could either go left, through the patchwork quilt of countries behind the Tatra mountains on the Polish-Slovakian border or go right, also encountering the Carpathian mountain range but navigating the bulks of Ukraine, Romania and Bulgaria before ending in Greece.'}
+
+    ]
   }
 })
